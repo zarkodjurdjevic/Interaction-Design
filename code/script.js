c8d1c2ff6cf02bcbf1b7e12e666e70dc2afef8b0
unknown <31924@ma-web.nl>
2022-06-12 21:39:51 +0200

    skills,contact,home pages

--- code/script.js
+++ code/script.js
@@ -16,6 +16,24 @@ sr.reveal('.tekst1',{delay:350, origin:'left'})
 sr.reveal('.fotozarko',{delay:350, origin:'right'})
 sr.reveal('.overmij1',{delay:350, origin:'right'})
 
+let cirkel = document.querySelector(".cirkel"), 
+	progwaarde = document.querySelector(".progressie");
+
+let progbeginwaarde = 0,
+	progeindwaarde = 80,
+	speed = 10;
+
+let progress = setInterval(() => {
+	progbeginwaarde++;
+	progwaarde.textContent = `${progbeginwaarde}%`
+	cirkel.style.background = `conic-gradient(#33D67E ${progbeginwaarde * 3.6}deg, #ededed 0deg)`;
+	if (progbeginwaarde == progeindwaarde){
+		clearInterval(progress);
+	}
+	console.log(progbeginwaarde);
+
+}, speed);
+
 
 
 
